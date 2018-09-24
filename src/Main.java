7738f729a060b1f0ea2e5a76095f64a774ccf5c5
kthong3 <kthongsakounh@gmail.com>
2018-09-24 12:09:16 -0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // In an array of arrays, e.g. given [[], [1, 2, 3], [4, 5], [], [], [6, 7], [8], [9, 10], [], []]
+        // print: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10.
+        // input: array of arrays
+        // output: strings
+    }
+}
